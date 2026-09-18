301ec484662621e2b702f2f3ae73d0743c136b18
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-18 16:59:50 -0500

    feat(matrices): declare a 3x3 integer matrix and initialize it with values 1 to 9

--- src/main/java/com/mjallerp/service/Matrices.java
+++ src/main/java/com/mjallerp/service/Matrices.java
@@ -13,4 +13,25 @@ public class Matrices {
             }
         }
     }
+
+    public void mostrarComoTabla() {
+        System.out.println("Matriz 3x3 (tabla):");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void recorrerPorColumnas() {
+        System.out.println("Recorrido por columnas:");
+        for (int j = 0; j < matriz[0].length; j++) {
+            System.out.print("Columna " + j + ": ");
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
